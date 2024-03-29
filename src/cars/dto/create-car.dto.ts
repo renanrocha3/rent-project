8ea00d376a64384f04d82a25623fc84306870807
renanrocha3 <renan.rocha@alunos.unicesumar.edu.br>
2024-03-28 22:14:58 -0300

    Add files via upload

--- src/cars/dto/create-car.dto.ts
+++ src/cars/dto/create-car.dto.ts
@@ -1,24 +1,29 @@
 import { IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator'
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateCarDto {
+    @ApiProperty()
     @IsString()
     @IsNotEmpty()
     @MinLength(7)
     @MaxLength(7)
     plate: string
 
+    @ApiProperty()
     @IsString()
     @IsNotEmpty()
     @MinLength(3)
     @MaxLength(50)
     color: string
 
+    @ApiProperty()
     @IsString()
     @IsNotEmpty()
     @MinLength(3)
     @MaxLength(50)
     model: string
 
+    @ApiProperty()
     @IsString()
     @IsNotEmpty()
     @MinLength(3)
--- src/users/dtos/create-user-dto.ts
+++ src/users/dtos/create-user-dto.ts
@@ -1,23 +1,28 @@
 import { IsEmail, IsInt, IsNotEmpty, IsString, Max, MaxLength, MinLength } from 'class-validator'
+import { ApiProperty } from '@nestjs/swagger';
 
 
 export class CreateUserDto {
+    @ApiProperty()
     @IsString()
     @IsNotEmpty()
     @MinLength(3)
     @MaxLength(50)
     name: string
 
+    @ApiProperty()
     @IsString()
     @IsNotEmpty()
     @MinLength(4)
     @MaxLength(20)
     password: string
 
+    @ApiProperty()
     @IsInt()
     @Max(100)
     age: number
 
+    @ApiProperty()
     @IsEmail()
     email: string
 }
